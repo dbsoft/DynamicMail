e106fe5e3ad34594b04eefa7332e9a555bf25f68
Brian Smith <brian@dbsoft.org>
2020-11-29 04:29:31 -0600

    GTK: SWitch to using GResrouce for images on Unix.

---
+++ gtk/resources.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+mkdir -p "resources"
+    
+ cp -f mac/handyftp.png "resources/155.png"
+ cp -f mac/exit.png "resources/251.png"
+ cp -f mac/connect.png "resources/335.png"
+ cp -f mac/disconnect.png "resources/336.png"
+ cp -f mac/queue.png "resources/337.png"
+ cp -f mac/unqueue.png "resources/338.png"
+ cp -f mac/flush.png "resources/346.png"
+ cp -f mac/save.png "resources/350.png"
+ cp -f mac/unsave.png "resources/351.png"
+ cp -f mac/admin.png "resources/349.png"
+ cp -f mac/change.png "resources/309.png"
+ cp -f mac/help.png "resources/268.png"
+ cp -f mac/about.png "resources/269.png"
+ cp -f mac/preferences.png "resources/255.png"
+ cp -f mac/remtab.png "resources/353.png"
+ cp -f mac/newtab.png "resources/355.png"
+ cp -f mac/FILE.png "resources/356.png"
+ cp -f mac/FOLDER.png "resources/357.png"
+ cp -f mac/LINK.png "resources/365.png"
+
